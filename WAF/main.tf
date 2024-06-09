6936c5b77861a4872069ac2f9b544c18b14b3eaf
D-Mwanth <dmwanthi2@gmail.com>
2024-06-09 15:09:39 +0300

    modified iam module

--- WAF/main.tf
+++ WAF/main.tf
@@ -14,10 +14,6 @@ resource "aws_wafv2_web_acl" "this" {
     action {
       block {}
     }
-    
-    override_action {
-      none {}
-    }
 
     statement {
       managed_rule_group_statement {
